cda9ddfffda7426251401c935fd5aac5663da78b
Guilherme Vieira <guivpw68@gmail.com>
2022-04-11 02:59:54 -0300

    refactor: remove schemas

--- src/app.ts
+++ src/app.ts
@@ -3,11 +3,9 @@ import 'reflect-metadata'
 import fastify from 'fastify'
 import cors from 'fastify-cors'
 import helmet from 'fastify-helmet'
-import swagger from 'fastify-swagger'
-
-import { userRouter } from '@modules/users'
 
 import { IS_PROD } from './constants/envs'
+import { userRouter } from './modules/users'
 
 export const app = fastify({
   logger: {
@@ -23,39 +21,4 @@ export const app = fastify({
 app.register(cors, { allowedHeaders: '*' })
 app.register(helmet)
 
-app.register(swagger, {
-  routePrefix: '/documentation',
-  swagger: {
-    info: {
-      title: 'Documentação da API de Eventos',
-      description: 'Testando a API',
-      version: '1.0.0'
-    },
-    host: 'localhost',
-    schemes: ['http'],
-    consumes: ['application/json'],
-    produces: ['application/json'],
-    tags: [
-      { name: 'users', description: 'Endpoints relacionados aos Usuários' },
-      { name: 'events', description: 'Endpoints relacionados aos Eventos' },
-      { name: 'creators', description: 'Endpoints relacionados aos Criadores de Eventos' }
-    ],
-    securityDefinitions: {
-      token: {
-        type: 'apiKey',
-        name: 'bearer',
-        in: 'header',
-        description: 'Token de segurança gerado ao fazer login'
-      }
-    }
-  },
-  staticCSP: true,
-  exposeRoute: true
-})
-
-app.addSchema({
-  $id: 'Error',
-  type: 'string'
-})
-
-app.register(userRouter, { prefix: '/users' })
+app.register(userRouter, { prefix: '/user' })
