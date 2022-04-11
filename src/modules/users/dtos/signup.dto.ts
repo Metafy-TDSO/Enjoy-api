92335330e66c6c67d3688cc6e352e4745054d8e0
Guilherme Vieira <guivpw68@gmail.com>
2022-04-11 00:48:04 -0300

    feat: add signup dto

---
+++ src/modules/users/dtos/signup.dto.ts
@@ -0,0 +1,34 @@
+import { IsDateString, IsEmail, IsOptional, IsString, Matches, MaxLength } from 'class-validator'
+
+import { createStringRequirements } from 'utils/regexp.utils'
+
+export class SignUpDto {
+  @IsString()
+  @MaxLength(72)
+  name!: string
+
+  @IsEmail()
+  @MaxLength(72)
+  email!: string
+
+  @IsDateString()
+  birth!: Date
+
+  @IsString()
+  @Matches(createStringRequirements(), {
+    message:
+      'Password too weak! Include at least 8 characters with at least one uppercase and lowercase letter'
+  })
+  password!: string
+
+  @IsString()
+  confirmPassword!: string
+
+  @IsString()
+  phone!: string
+
+  @IsString()
+  @MaxLength(72)
+  @IsOptional()
+  avatarUrl?: string
+}
