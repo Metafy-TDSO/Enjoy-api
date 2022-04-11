1978cef03ab09f4201bee91fe3f6c3309a71ac7a
Guilherme Vieira <guivpw68@gmail.com>
2022-04-10 22:47:24 -0300

    feat: add fastify and middlewares

---
+++ src/app.ts
@@ -0,0 +1,20 @@
+import fastify from 'fastify'
+import { IS_DEV } from './constants/envs'
+import cors from 'fastify-cors'
+import helmet from 'fastify-helmet'
+import gracefullyShutDown from 'fastify-graceful-shutdown'
+
+export const app = fastify({
+  logger: {
+    prettyPrint: IS_DEV
+      ? {
+          translateTime: 'HH:MM:ss Z',
+          ignore: 'pid,hostname'
+        }
+      : false
+  }
+})
+
+app.register(cors, { allowedHeaders: '*' })
+app.register(helmet)
+app.register(gracefullyShutDown)
