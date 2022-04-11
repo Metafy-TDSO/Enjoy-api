8ef1d6b8ac101476e320bc630388f5b61d2c4b0e
Guilherme Vieira <guivpw68@gmail.com>
2022-04-11 02:16:28 -0300

    feat: add login controller

---
+++ src/modules/users/use-cases/login/login.controller.ts
@@ -0,0 +1,38 @@
+import { plainToInstance } from 'class-transformer'
+import { FastifyRequest, FastifyReply } from 'fastify'
+
+import { LoginDto } from '@modules/users/dtos'
+
+import { HttpError } from 'errors/http.errors'
+
+import { LoginUseCase } from './login.use-case'
+
+export class LoginController {
+  constructor(private loginUseCase: LoginUseCase) {}
+
+  async handle(request: FastifyRequest, reply: FastifyReply): Promise<FastifyReply> {
+    const input = request.body as LoginDto
+
+    try {
+      plainToInstance(LoginDto, input)
+    } catch (err) {
+      const { code, message } = err as HttpError
+
+      return reply.status(code ?? 400).send({
+        message: message || 'Invalid Arguments provided.'
+      })
+    }
+
+    try {
+      const result = await this.loginUseCase.execute(input)
+
+      return await reply.status(200).send(result)
+    } catch (err) {
+      const { code, message } = err as HttpError
+
+      return reply.status(code ?? 400).send({
+        message: message || 'Unexpected error.'
+      })
+    }
+  }
+}
