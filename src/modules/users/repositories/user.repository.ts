4de228392fa678c1dc690aab77948ff6ee307f99
Guilherme Vieira <guivpw68@gmail.com>
2022-04-11 00:47:26 -0300

    feat: add user repository

---
+++ src/modules/users/repositories/user.repository.ts
@@ -0,0 +1,31 @@
+import { PrismaClient } from '@prisma/client'
+
+import { prisma } from '@database'
+
+import { User } from '../models'
+
+export class UserRepository {
+  private prisma: PrismaClient = prisma
+
+  async findByEmail(email: string): Promise<User | null> {
+    const user = this.prisma.user.findUnique({ where: { email } })
+
+    return user
+  }
+
+  async findById(id: number): Promise<User | null> {
+    const user = this.prisma.user.findUnique({ where: { id } })
+
+    return user
+  }
+
+  async save(user: Omit<User, 'id' | 'updatedAt' | 'createdAt'>): Promise<User> {
+    return this.prisma.user.create({ data: user })
+  }
+
+  async exists({ id, email }: { id?: number; email?: string }) {
+    const countOfUsers = await this.prisma.user.count({ where: { id, email } })
+
+    return countOfUsers > 0
+  }
+}
