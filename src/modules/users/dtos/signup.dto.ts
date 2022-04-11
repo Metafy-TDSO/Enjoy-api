0e7d8275f2f56902ee2dfdc8411bf70c2f1e6846
Guilherme Vieira <guivpw68@gmail.com>
2022-04-11 03:54:42 -0300

    feat: add custom error messages

--- src/modules/users/dtos/signup.dto.ts
+++ src/modules/users/dtos/signup.dto.ts
@@ -1,34 +1,45 @@
-import { IsDateString, IsEmail, IsOptional, IsString, Matches, MaxLength } from 'class-validator'
+import {
+  IsDateString,
+  IsEmail,
+  IsOptional,
+  IsPhoneNumber,
+  IsString,
+  IsUrl,
+  Matches,
+  MaxLength
+} from 'class-validator'
 
 import { createStringRequirements } from 'utils/regexp.util'
 
 export class SignUpDto {
-  @IsString()
+  @IsString({ message: 'Insert a valid name!' })
   @MaxLength(72)
   name!: string
 
-  @IsEmail()
+  @IsEmail({}, { message: 'Insert a valid email!' })
   @MaxLength(72)
   email!: string
 
-  @IsDateString()
+  @IsDateString(
+    { format: 'DD/MM/YYYY' },
+    { message: 'Insert a valid date in the format: DD/MM/YYYY!' }
+  )
   birth!: Date
 
-  @IsString()
+  @IsString({ message: 'Insert a valid password!' })
   @Matches(createStringRequirements(), {
     message:
       'Password too weak! Include at least 8 characters with at least one uppercase and lowercase letter'
   })
   password!: string
 
-  @IsString()
+  @IsString({ message: 'You must confirm your password!' })
   confirmPassword!: string
 
-  @IsString()
+  @IsPhoneNumber('BR', { message: 'Insert a valid phone!' })
   phone!: string
 
-  @IsString()
-  @MaxLength(72)
+  @IsUrl({}, { message: 'Insert a valid image url!' })
   @IsOptional()
   avatarUrl?: string
 }
