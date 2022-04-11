0b8c3b4175b7cfdef0c0dba47ccc7961d40607d9
Guilherme Vieira <guivpw68@gmail.com>
2022-04-11 02:17:27 -0300

    feat: add signup route with swagger schema

--- src/modules/users/user.router.ts
+++ src/modules/users/user.router.ts
@@ -1,7 +1,7 @@
 import { FastifyInstance } from 'fastify'
 
 import { UserRepository } from './repositories'
-import { SignUpUseCase, SignUpController } from './use-cases/signup'
+import { SignUpController, SignUpUseCase } from './use-cases/signup'
 
 // Repositories
 const userRepository = new UserRepository()
@@ -10,5 +10,57 @@ const userRepository = new UserRepository()
 const signUpUseCase = new SignUpUseCase(userRepository)
 
 export const userRouter = (app: FastifyInstance) => {
-  app.get('/signup', async (req, rep) => new SignUpController(signUpUseCase).handle(req, rep))
+  app.addSchema({
+    $id: 'User',
+    type: 'object',
+    properties: {
+      id: { type: 'integer' },
+      name: { type: 'string', example: 'Guilherme Vieira' },
+      email: { type: 'string', format: 'email', example: 'example@gmail.com' },
+      birth: { type: 'string', format: 'date', example: '2002-08-02' },
+      phone: { type: 'string', minLength: 11, maxLength: 10, example: '11912345678' },
+      avatarUrl: { type: 'string', format: 'uri' },
+      createdAt: { type: 'string', format: 'date-time', example: '2022-04-11' },
+      updatedAt: { type: 'string', format: 'date-time', default: null, example: '2022-04-11' }
+    },
+    required: ['id', 'name', 'email', 'birth', 'phone']
+  })
+
+  app.post(
+    '/signup',
+    {
+      schema: {
+        response: {
+          201: {
+            description: 'Successful signup',
+            type: 'object',
+            properties: {
+              user: {
+                $ref: 'User#'
+              },
+              token: { type: 'string' }
+            }
+          },
+          400: {
+            type: 'string',
+            $ref: '#Error'
+          }
+        },
+        body: {
+          type: 'object',
+          properties: {
+            name: { type: 'string', example: 'Guilherme Vieira' },
+            email: { type: 'string', format: 'email', example: 'example@gmail.com' },
+            birth: { type: 'string', format: 'date', example: '2002-08-02' },
+            password: { type: 'string', format: 'password', example: 'Guigui123##' },
+            confirmPassword: { type: 'string', format: 'password', example: 'Guigui123##' },
+            phone: { type: 'string', minLength: 11, maxLength: 10, example: '11912345678' },
+            avatarUrl: { type: 'string', format: 'uri' }
+          },
+          required: ['name', 'email', 'birth', 'password', 'confirmPasssword', 'phone']
+        }
+      }
+    },
+    async (req, rep) => new SignUpController(signUpUseCase).handle(req, rep)
+  )
 }
