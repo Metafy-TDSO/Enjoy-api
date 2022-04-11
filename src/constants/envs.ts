5ae5998c52b4e80d49ddc7fc155f4386b95c3cbb
Guilherme Vieira <guivpw68@gmail.com>
2022-04-11 02:32:35 -0300

    refactor: update env statement

--- src/constants/envs.ts
+++ src/constants/envs.ts
@@ -1,3 +1,3 @@
-export const IS_DEV = process.env.NODE_ENV === 'development'
+export const IS_PROD = process.env.NODE_ENV === 'production'
 export const PORT = Number(process.env.PORT ?? 3000)
 export const JWT_SECRET = process.env.SECRET ?? 'secret123'
