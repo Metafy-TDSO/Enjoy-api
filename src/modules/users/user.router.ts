700d6c5e3b93339fcf1aa6a66f12f17decc0ecc0
Guilherme Vieira <guivpw68@gmail.com>
2022-04-12 00:24:23 -0300

    feat: add create creator route

--- src/modules/users/user.router.ts
+++ src/modules/users/user.router.ts
@@ -1,19 +1,26 @@
 import { FastifyPluginCallback } from 'fastify'
 
-import { UserRepository } from './repositories'
+import { CreatorRepository, UserRepository } from './repositories'
+import { CreateCreatorController, CreateCreatorUseCase } from './use-cases/create-creator'
 import { LoginController, LoginUseCase } from './use-cases/login'
 import { SignUpController, SignUpUseCase } from './use-cases/signup'
 
 // Repositories
 const userRepository = new UserRepository()
+const creatorRepository = new CreatorRepository()
 
 // Use Cases
 const signUpUseCase = new SignUpUseCase(userRepository)
 const loginUseCase = new LoginUseCase(userRepository)
+const createCreatorUseCase = new CreateCreatorUseCase(userRepository, creatorRepository)
 
 export const userRouter: FastifyPluginCallback = (app, _opts, done) => {
   app.post('/signup', async (req, rep) => new SignUpController(signUpUseCase).handle(req, rep))
   app.post('/login', async (req, rep) => new LoginController(loginUseCase).handle(req, rep))
+
+  app.post('/:idUser/creator', async (req, rep) =>
+    new CreateCreatorController(createCreatorUseCase).handle(req, rep)
+  )
 
   done()
 }
