ec1b59d02b8153242ad55632de7c21fb1925cf14
Guilherme Vieira <guivpw68@gmail.com>
2022-04-12 00:09:48 -0300

    feat: add creator repository

---
+++ src/modules/users/repositories/creator.repository.ts
@@ -0,0 +1,25 @@
+import { PrismaClient } from '@prisma/client'
+
+import { prisma } from '@common/database'
+
+import { Creator } from '../models'
+
+export class CreatorRepository {
+  private prisma: PrismaClient = prisma
+
+  async findById(id: number): Promise<Creator | null> {
+    const creator = this.prisma.creator.findUnique({ where: { id } })
+
+    return creator
+  }
+
+  async findByUserId(idUser: number): Promise<Creator | null> {
+    const creator = this.prisma.creator.findUnique({ where: { idUser } })
+
+    return creator
+  }
+
+  async save(creator: Omit<Creator, 'id' | 'updatedAt' | 'createdAt'>): Promise<Creator> {
+    return this.prisma.creator.create({ data: creator })
+  }
+}
