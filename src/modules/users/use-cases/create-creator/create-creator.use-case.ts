3b1e479f5c674e8d98b42ce2a3bbdccde6a2fff5
Guilherme Vieira <guivpw68@gmail.com>
2022-04-12 00:17:48 -0300

    feat: add create creator use case

---
+++ src/modules/users/use-cases/create-creator/create-creator.use-case.ts
@@ -0,0 +1,34 @@
+import { BadRequestError, NotFoundError } from '@common/errors/http.errors'
+
+import { CreateCreatorDto } from '../../dtos'
+import { Creator } from '../../models'
+import { UserRepository, CreatorRepository } from '../../repositories'
+
+export class CreateCreatorUseCase {
+  constructor(
+    private readonly userRepository: UserRepository,
+    private readonly creatorRepository: CreatorRepository
+  ) {}
+
+  async execute(input: CreateCreatorDto): Promise<{ creator: Creator }> {
+    const { idUser } = input
+
+    const userExists = await this.userRepository.exists({ id: idUser })
+
+    if (!userExists) {
+      throw NotFoundError(`This user doesn't exist.`)
+    }
+
+    const creatorExists = await this.creatorRepository.exists({ idUser })
+
+    if (!creatorExists) {
+      throw BadRequestError('This user already is a creator.')
+    }
+
+    const createdCreator = await this.creatorRepository.save({ idUser })
+
+    return {
+      creator: createdCreator
+    }
+  }
+}
