9df99eadea9041356a3fa558597bcd8d5b348737
Guilherme Vieira <guivpw68@gmail.com>
2022-04-12 00:17:38 -0300

    feat: add exists method

--- src/modules/users/repositories/creator.repository.ts
+++ src/modules/users/repositories/creator.repository.ts
@@ -22,4 +22,10 @@ export class CreatorRepository {
   async save(creator: Omit<Creator, 'id' | 'updatedAt' | 'createdAt'>): Promise<Creator> {
     return this.prisma.creator.create({ data: creator })
   }
+
+  async exists({ id, idUser }: { id?: number; idUser?: number }) {
+    const countOfCreators = await this.prisma.creator.count({ where: { id, idUser } })
+
+    return countOfCreators > 0
+  }
 }
