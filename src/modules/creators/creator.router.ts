a058a0bec5e1a1a38d3740f9b0992c5e095ad603
Guilherme Vieira <guivpw68@gmail.com>
2022-04-14 00:02:41 -0300

    feat: add creator router

---
+++ src/modules/creators/creator.router.ts
@@ -0,0 +1,24 @@
+import { FastifyPluginCallback } from 'fastify'
+
+import { authenticationMiddleware, isAccountOwnerMiddleware } from '@common/middlewares'
+
+import { EventRepository } from '@modules/events/repositories'
+import { CreateEventUseCase, CreateEventController } from '@modules/events/use-cases'
+import { CreatorRepository } from '@modules/users/repositories'
+
+// Repositories
+const eventRepository = new EventRepository()
+const creatorRepository = new CreatorRepository()
+
+// Use Cases
+const createEventUseCase = new CreateEventUseCase(eventRepository, creatorRepository)
+
+export const creatorRouter: FastifyPluginCallback = (app, _opts, done) => {
+  app.post(
+    '/:idCreator/event',
+    { preHandler: [authenticationMiddleware, isAccountOwnerMiddleware] },
+    async (req, rep) => new CreateEventController(createEventUseCase).handle(req, rep)
+  )
+
+  done()
+}
