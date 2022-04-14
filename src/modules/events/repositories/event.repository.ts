ad75f9213189a43f26465b4c7f78f3facf3e8b46
Guilherme Vieira <guivpw68@gmail.com>
2022-04-13 23:40:20 -0300

    feat: add events repository

--- src/modules/events/repositories/event.repository.ts
+++ src/modules/events/repositories/event.repository.ts
@@ -1,8 +1,14 @@
 import { Prisma, PrismaClient } from '@prisma/client'
 
 import { prisma } from '@common/database'
+import { OmitDbAttrs } from '@common/types/omit-db-attrs.type'
 
 import { Event } from '../models'
+
+interface Location {
+  latitude: number
+  longitude: number
+}
 
 export class EventRepository {
   private prisma: PrismaClient = prisma
@@ -23,5 +29,47 @@ export class EventRepository {
     const countOfEvents = await this.prisma.event.count({ where: { id, idCreator } })
 
     return countOfEvents > 0
+  }
+
+  async findAllEventsInRadius({
+    meters = 10000,
+    userLocation: { latitude, longitude }
+  }: {
+    meters?: number
+    userLocation: Location
+  }) {
+    const userGeoPoint = `ST_GeomFromText('POINT(${latitude} ${longitude})', 4326)`
+
+    const foundEvents = await this.prisma.$queryRaw<Event[]>`
+      SELECT * from tbl_evento WHERE ST_Distance(${userGeoPoint}, lc_localizacao, 'metre') <= ${meters}
+    `
+
+    return foundEvents
+  }
+
+  async save({
+    description,
+    endsAt,
+    idCreator,
+    latitude,
+    longitude,
+    name,
+    startAt
+  }: OmitDbAttrs<Omit<Event, 'localization'>> & Location): Promise<Event | null> {
+    try {
+      const geoPoints = `ST_GeomFromText('POINT(${latitude} ${longitude})', 4326)`
+
+      await this.prisma.$executeRaw`
+        INSERT INTO tbl_evento (ds_nome, ds_descricao, dt_inicio, dt_fim, id_criador, lc_localizacao) 
+        VALUES (${name}, ${description}, ${startAt.toISOString()}, ${endsAt.toISOString()}, ${idCreator}, ${geoPoints})
+      `
+
+      const createdEvent = await this.prisma.event.findFirst({ where: { idCreator, name } })
+
+      return createdEvent
+    } catch (error) {
+      console.log(error)
+      return null
+    }
   }
 }
