9cefb4d3c22ccc931adec4fd9b6b385a307a4a0d
Guilherme Vieira <guivpw68@gmail.com>
2022-04-15 18:46:54 -0300

    feat: improve typing and messaging

--- src/modules/gateways/handlers/events.handler.ts
+++ src/modules/gateways/handlers/events.handler.ts
@@ -1,24 +1,20 @@
-import { Server, Socket } from 'socket.io'
-
 import { Location } from '@modules/events/models'
-import { JoinedEventCreator } from '@modules/events/repositories'
 import { FindManyEventsUseCase } from '@modules/events/use-cases'
 
-export interface EventHandlerEvents {
-  'event:search-near': (message: Location) => void
-  top: () => void
-}
+import { ServerIoData, SocketIoData } from '../types/socket.type'
 
 export class EventsHandler {
   constructor(
-    private readonly io: Server,
-    private readonly socket: Socket,
+    private io: ServerIoData,
+    private socket: SocketIoData,
     private readonly findManyEventsUseCase: FindManyEventsUseCase
   ) {}
 
-  public async searchCloseEvents(location: Location): Promise<{ events: JoinedEventCreator[] }> {
+  public async searchCloseEvents(location: Location) {
     const { latitude, longitude } = location
 
-    return this.findManyEventsUseCase.execute({ latitude, longitude })
+    const foundEvents = await this.findManyEventsUseCase.execute({ latitude, longitude })
+
+    this.socket.emit('server:event:search-near', foundEvents)
   }
 }
