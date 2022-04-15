42bc51b974d24a62bd24d81d33d7df71a13fde1c
Guilherme Vieira <guivpw68@gmail.com>
2022-04-15 18:56:53 -0300

    feat: add find many events route

--- src/modules/events/event.router.ts
+++ src/modules/events/event.router.ts
@@ -5,7 +5,12 @@ import { authenticationMiddleware } from '@common/middlewares'
 import { CreatorRepository } from '@modules/users/repositories'
 
 import { EventRepository } from './repositories'
-import { CreateEventController, CreateEventUseCase } from './use-cases'
+import {
+  CreateEventController,
+  CreateEventUseCase,
+  FindManyEventsController,
+  FindManyEventsUseCase
+} from './use-cases'
 
 // Repositories
 const eventRepository = new EventRepository()
@@ -13,12 +18,13 @@ const creatorRepository = new CreatorRepository()
 
 // Use Cases
 const createEventUseCase = new CreateEventUseCase(eventRepository, creatorRepository)
+const findManyEventsUseCase = new FindManyEventsUseCase(eventRepository)
 
 export const eventRouter: FastifyPluginCallback = (app, _opts, done) => {
   app.post('/', { preHandler: [authenticationMiddleware] }, (req, rep) =>
     new CreateEventController(createEventUseCase).handle(req, rep)
   )
-  app.get('/', (req, rep) => new CreateEventController(createEventUseCase).handle(req, rep))
+  app.get('/', (req, rep) => new FindManyEventsController(findManyEventsUseCase).handle(req, rep))
 
   done()
 }
