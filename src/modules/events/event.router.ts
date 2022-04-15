0751ecd0ecb9d3f0fc7167adca7a58637c36abc5
Guilherme Vieira <guivpw68@gmail.com>
2022-04-15 17:06:15 -0300

    feat: add event routes

--- src/modules/events/event.router.ts
+++ src/modules/events/event.router.ts
@@ -1,12 +1,20 @@
 import { FastifyPluginCallback } from 'fastify'
 
-// import { EventRepository } from './repositories'
+import { CreatorRepository } from '@modules/users/repositories'
+
+import { EventRepository } from './repositories'
+import { CreateEventController, CreateEventUseCase } from './use-cases'
 
 // Repositories
-// const eventRepository = new EventRepository()
+const eventRepository = new EventRepository()
+const creatorRepository = new CreatorRepository()
 
 // Use Cases
+const createEventUseCase = new CreateEventUseCase(eventRepository, creatorRepository)
 
 export const eventRouter: FastifyPluginCallback = (app, _opts, done) => {
+  app.post('/', (req, rep) => new CreateEventController(createEventUseCase).handle(req, rep))
+  app.get('/', (req, rep) => new CreateEventController(createEventUseCase).handle(req, rep))
+
   done()
 }
